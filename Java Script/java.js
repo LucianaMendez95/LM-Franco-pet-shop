69ae7baa234247338ca39e77088e8123e9cb7138
Luciana Mendez <lumendez@MacBook.local>
2020-08-07 16:24:29 -0300

    formulario

--- Java Script/java.js
+++ Java Script/java.js
@@ -47,7 +47,7 @@ $(document).ready(
 
     boton.addEventListener("click", e =>{
        e.preventDefault()
-       if(telefono.value.length > 7 && nombre.value.length > 2 && apellido.value.length > 2 && mail.value.length > 6 && asunto.value.length > 2 && consulta.value.length > 5 && (perro.checked == 1 || gato.checked == 1)) {
+       if(telefono.value.length > 1 && nombre.value.length > 1 && apellido.value.length > 1 && mail.value.length > 1 && asunto.value.length > 1 && consulta.value.length > 1 && (perro.checked == 1 || gato.checked == 1)) {
           swal(`Muchas gracias ${nombre.value}!`, `Su consulta fue enviada correctamente, a la brevedad recibiras una respuesta al siguiente mail: ${mail.value}`, "success");
          } else{
             swal("Disculpa!", "Debes completar todos los campos correctamente para poder enviar el formulario!");
